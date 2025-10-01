88e3effa0715e86b0452b1727d8f6b346e234780
Fabio De Carvalho Almeida <fabialme@c3r6p4.42sp.org.br>
2025-10-01 15:10:06 -0300

    refactored atdbl

--- src/ft_atdbl.c
+++ src/ft_atdbl.c
@@ -14,23 +14,28 @@
 
 double	ft_atdbl(const char *nptr)
 {
-	double		sum;
-	char	sign;
+	long		integer_part;
+	double		fractional_part;
+	double	pow;
+	char		sign;
 
-	sum = 0;
+	integer_part = 0;
+	fractional_part = 0;
 	sign = 1;
+	pow = 1;
 	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
 		nptr++;
-	if (*nptr == '-' || *nptr == '+')
-	{
-		if (*nptr == '-')
+	while (*nptr == '-' || *nptr == '+')
+		if (*nptr++ == '-')
 			sign *= -1;
+	while (*nptr != '.' && *nptr)
+		integer_part = (integer_part * 10) + (*nptr++ - 48);
+	if (*nptr == '.')
 		nptr++;
-	}
-	while (*nptr >= '0' && *nptr <= '9')
+	while (*nptr)
+		pow /= 10;
 	{
-		sum = (sum * 10) + (*nptr - 48);
-		nptr++;
+		fractional_part = fractional_part + (*nptr - 48) * pow;
 	}
-	return (sum * sign);
+	return (integer_part + fractional_part * sign);
 }
